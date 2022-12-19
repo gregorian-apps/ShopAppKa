b8ccd4538df86f44bd2472697417e0859e781369
Grzegorz Chojnacki <g.chojnacki0@gmail.com>
2022-12-19 23:41:39 +0100

    Initial Commit

---
+++ src/main/java/com/shop/list/shopappka/ShopAppKaApplication.java
@@ -0,0 +1,13 @@
+package com.shop.list.shopappka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAppKaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAppKaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/list/shopappka/ShopAppKaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.list.shopappka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAppKaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
