2966d2685d14d3c713ded556917337d39de9c082
Grzegorz Chojnacki <g.chojnacki0@gmail.com>
2023-01-08 18:52:09 +0100

    Add update operation for user

--- src/main/java/com/shop/list/shopappka/services/UserService.java
+++ src/main/java/com/shop/list/shopappka/services/UserService.java
@@ -2,9 +2,12 @@ package com.shop.list.shopappka.services;
 
 import com.shop.list.shopappka.exceptions.UserException;
 import com.shop.list.shopappka.models.domain.User;
+import com.shop.list.shopappka.payload.UpdateUser;
 import com.shop.list.shopappka.payload.UserRequest;
 import com.shop.list.shopappka.repositories.UserRepository;
+import com.shop.list.shopappka.utils.FormatUtils;
 import jakarta.validation.constraints.NotNull;
+import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -43,5 +46,22 @@ public class UserService {
                 .email(userRequest.getEmail())
                 .build();
         return userRepository.save(user);
+    }
+
+    public void updateUserData(@NonNull UpdateUser updateUser, String id) {
+        UUID uuid = FormatUtils.getUUIDFromString(id);
+        assert uuid != null;
+        Optional<User> existingUser = userRepository.findById(uuid);
+
+        if(existingUser.isPresent()) {
+            User user = existingUser.get();
+            user.setEmail(updateUser.getEmail());
+            user.setLogin(updateUser.getLogin());
+            user.setFirstName(updateUser.getFirstName());
+            userRepository.save(user);
+        } else {
+            log.warn("User with email {} doesn't exist in the system", updateUser.getEmail());
+            throw new UserException("User doesn't exist in the system with email: " + updateUser.getEmail());
+        }
     }
 }
