810cfbc5f27ff200dde06ad9884f43f1a7e241b5
Grzegorz Chojnacki <g.chojnacki0@gmail.com>
2023-01-15 19:57:32 +0100

    Fix build

--- src/test/java/com/shop/list/shopappka/repositories/UserRepositoryTest.java
+++ src/test/java/com/shop/list/shopappka/repositories/UserRepositoryTest.java
@@ -1,19 +1,25 @@
 package com.shop.list.shopappka.repositories;
 
 import com.shop.list.shopappka.models.domain.User;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.*;
+
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private TestEntityManager entityManager;
 
     @Test
     void shouldReturnSavedUserAfterSaveOperation(){
